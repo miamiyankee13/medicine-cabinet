4ae29f3ad8d7d90ba90c460f306128c52e07e879
Anthony Damico <anthony.damico721@gmail.com>
2018-12-11 16:33:17 -0500

    added initial testing functionality

--- server.js
+++ server.js
@@ -20,7 +20,42 @@ app.use( (req, res, next) => {
 //Serve static files from public folder
 app.use(express.static('public'));
 
-//Open port for requsts
-app.listen(process.env.PORT || 8080, () => {
-    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+//Declare global server object
+let server;
+
+//Start server and return a Promise
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app
+      .listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+      .on("error", err => {
+        reject(err);
+      });
+  });
+}
+
+//Close server and return a Promise
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+//Allows access to 'node server.js' & other test files if imported/exported
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
