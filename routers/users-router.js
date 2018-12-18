fac4a9bddf1867884fcadf483fa2cd799ffa224c
Anthony Damico <anthony.damico721@gmail.com>
2018-12-18 12:49:35 -0500

    set up initial endpoints for /users/strains

--- routers/users-router.js
+++ routers/users-router.js
@@ -2,6 +2,7 @@
 //Import dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 //Declare JSON parser
 const jsonParser = bodyParser.json();
@@ -11,6 +12,9 @@ const { User } = require('../models');
 
 //Create router instance
 const router = express.Router();
+
+//Declare JWT strategy middleware
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
 //POST route handler to register a new user
 //-validate userName & password fields
@@ -112,6 +116,68 @@ router.post('/', jsonParser, (req, res) => {
         console.error(err);
         res.status(500).json({ message: 'Internal server error' });
     });
+});
+
+//GET rout handler for all user's strains
+router.get('/strains', jwtAuth, (req, res) => {
+    User.findOne({userName: req.user.userName}, {strains}).then(strains => {
+        res.status(200).json({ strains: strains.map(strain => strain.serialize())});
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+//GET route hanlder for individual user's strain
+router.get('/strains/:id', jwtAuth, (req, res) => {
+    User.findOne({userName: req.user.userName}, {strains: { $elemMatch: req.params.id} }).then(strain => {
+        res.status(200).json(strain.serialize());
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+//PUT route handler for adding a strain to a user
+router.put('/strains/:id', jwtAuth, (req, res) => {
+    User.updateOne({userName: req.user.userName}, { $push: { strains: req.params.id } }, { new: true }).then(user => {
+        res.status(200).json(user.serialize());
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    });
+});
+
+//POST route handler for adding a comment to a user's individual strain
+router.post('/strains/:id', jwtAuth, jsonParser, (req, res) => {
+    User.updateOne({userName: req.user.userName, strains: { $elemMatch: req.params.id }}, 
+        { $push: { "strains.$.comments": req.body.comment } }, { new: true }).then(user => {
+            res.status(200).json(user.serialize());
+        }).catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        })
+});
+
+//DELETE route handler for removing a strain from a user
+router.delete('/strains/:id', jwtAuth, (req, res) => {
+    User.updateOne({userName: req.user.userName}, { $pull: { strains: req.params.id} }, { new: true }).then(user => {
+        res.status(200).json(user.serialize());
+    }).catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    })
+});
+
+//DELETE route handler for removing a comment from a user's indivudal strain
+router.delete('/strains/:id/:comment', jwtAuth, (req, res) => {
+    User.updateOne({userName: userName, strains: { $elemMatch: req.params.id }}, 
+        { $pull: { "strains.$.comments": { $elemMatch: req.params.comment } } }, { new: true }).then(user => {
+            res.status(200).json(user.serialize());
+        }).catch(err => {
+            console.error(err);
+            res.status(500).json({ message: 'Internal server error' });
+        })
 });
 
 module.exports = router;
