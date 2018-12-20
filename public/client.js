ae814e76eb991316a60ba0524c674f95e7de96aa
Anthony Damico <anthony.damico721@gmail.com>
2018-12-20 16:42:13 -0500

    complete add & remove strains to cabinet functionality

--- public/client.js
+++ public/client.js
@@ -58,23 +58,49 @@ function getUserStrains() {
     $.ajax(settings).then(results => {
         STATE.userStrains = results.strains;
         displayCabinet();
-    }).catch(displayEmptyCabinet);
+    }).catch(displayError);
     
     console.log(STATE);
+}
+
+function addStrainToCabinet(id) {
+    const settings = {
+        url: `/users/strains/${id}`,
+        headers: {"Authorization": `Bearer ${STATE.token}`},
+        dataType: 'json',
+        type: 'PUT'
+    }
+
+    $.ajax(settings).then(() => {
+        getUserStrains();
+    }).catch(displayError);
+}
+
+function removeStrainFromCabinet(id) {
+    const settings = {
+        url: `/users/strains/${id}`,
+        headers: {"Authorization": `Bearer ${STATE.token}`},
+        dataType: 'json',
+        type: 'DELETE'
+    }
+
+    $.ajax(settings).then(() => {
+        getUserStrains();
+    }).catch(displayError);
 }
 
 
 //DISPLAY FUNCTIONS
 
 function displayStrainDropDown() {
-    const strainOptions = STATE.strains.map((strain, index) => renderStrainOptions(strain, index));
+    const strainOptions = STATE.strains.map((strain, index) => renderStrainOptions(strain, index)).join('');
     const strainDropDownHtml = `
         <form>
             <label for="strain-select">Select a Strain:</label>
             <select id="strain-select">
             ${strainOptions}
             </select>
-            <button type="submit">Add to Cabinet</button>
+            <button class="js-add-btn" type="submit">Add to Cabinet</button>
         </form>
     `;
 
@@ -93,21 +119,20 @@ function displayError() {
     $('.js-message').prop('hidden', false);
 }
 
-function displayEmptyCabinet() {
-    $('.js-message').text('Your cabinet is currently empty');
-    $('.js-message').prop('hidden', false);
-}
-
 //RENDERING FUNCTIONS
 
 function renderStrainOptions(strain, index) {
     const name = strain.name;
-    return `<option value="${name}" data-index="${index}">${name}</option>`;
+    return `<option value="${index}">${name}</option>`;
 }
 
 function renderCabinet(strain, index) {
     const name = strain.name;
-    return `<div class="cabinet-strain" data-index="${index}"><h2>${name}</h2></div>`;
+    return `<div class="cabinet-strain">
+                <h2>${name}</h2>
+                <button class="js-details-btn" data-index="${index}">Details</button>
+                <button class="js-remove-btn" data-index="${index}">Remove</button>
+            </div>`;
 }
 
 //EVENT LISTENERS
@@ -117,17 +142,34 @@ function submitUserLogin() {
         event.preventDefault();
         const userName = $('#username').val();
         const password = $('#password').val();
-        console.log(userName);
-        console.log(password);
         authenticateUser(userName, password);
     });
 }
 
+function submitAddToCabinet() {
+    $('body').on('click', '.js-add-btn', function(event) {
+        event.preventDefault();
+        const index = $('#strain-select').val();
+        const id = STATE.strains[index]._id;
+        addStrainToCabinet(id);
+    });
+}
+
+function submitRemoveFromCabinet() {
+    $('body').on('click', '.js-remove-btn', function(event) {
+        event.preventDefault();
+        const index = $(event.target).attr('data-index');
+        const id = STATE.userStrains[index]._id;
+        removeStrainFromCabinet(id);
+    });
+}
 
 //DOCUMENT READY FUNCTION
 
 function handleMedicineCabinet() {
     submitUserLogin();
+    submitAddToCabinet();
+    submitRemoveFromCabinet();
 }
 
 
