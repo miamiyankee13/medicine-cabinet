bb47b8a2f35caf258bc59be5e20abeeedc33a6ac
Anthony Damico <anthony.damico721@gmail.com>
2018-12-30 11:08:09 -0500

    edited create user error message

--- public/client.js
+++ public/client.js
@@ -115,7 +115,7 @@ function createUser(userName, password, firstName, lastName) {
         $('.js-message-success').prop('hidden', false);
         authenticateUser(userName, password);
     }).catch(err => {
-        displayError(err.responseJSON.message);
+        displayError(`${err.responseJSON.message} (${err.responseJSON.location})`);
     });
 }
 
